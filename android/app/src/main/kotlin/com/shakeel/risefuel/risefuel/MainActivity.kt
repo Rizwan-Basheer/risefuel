01ba4f868424ae30823a6af771c9550b720baf35
Kashif Ghafoor <usmanbg50@gmail.com>
2025-10-08 18:48:14 +0500

    Initial commit

---
+++ android/app/src/main/kotlin/com/shakeel/risefuel/risefuel/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.shakeel.risefuel.risefuel
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity : FlutterActivity()
---
+++ lib/core/constants/app_colors.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+
+class AppColors {
+  AppColors._();
+
+  static const Color primary = Color(0xFF4CAF50);
+  static const Color secondary = Color(0xFF2196F3);
+  static const Color lightBackground = Colors.white;
+  static const Color darkBackground = Color(0xFF101317);
+  static const Color lightSurface = Color(0xFFF4F7F9);
+  static const Color darkSurface = Color(0xFF1F242C);
+  static const Color favorite = Color(0xFFE91E63);
+}
+
